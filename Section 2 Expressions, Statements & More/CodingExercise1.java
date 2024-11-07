0d1a4f2e8384c8274e7ad9e93f2702550e855ba7
k9 <olafkrawczyk13@icloud.com>
2024-11-07 20:56:22 +0100

    Coding Exercise 1: Positive, Negative or Zero

---
+++ Section 2 Expressions, Statements & More/CodingExercise1.java
@@ -0,0 +1,14 @@
+public class CodingExercise1 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void checkNumber(int number) {
+        if (number > 0) System.out.println("positive");
+        if (number < 0) System.out.println("negative");
+        if (number == 0) System.out.println("zero");
+    }
+
+}
