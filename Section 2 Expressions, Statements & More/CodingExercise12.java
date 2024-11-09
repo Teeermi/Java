629fb18e3d4058798efe15137e4b84f5130fd74a
k9 <olafkrawczyk13@icloud.com>
2024-11-09 17:39:40 +0100

    Coding Exercise 12: Playing Cat

--- Section 2 Expressions, Statements & More/CodingExercise12.java
+++ Section 2 Expressions, Statements & More/CodingExercise12.java
@@ -1,2 +1,18 @@
 public class CodingExercise12 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer && temperature >= 25 && temperature <= 45) {
+            return true;
+        } else if (!summer && temperature >= 25 && temperature <= 35) {
+            return true
+        } else {
+            return false;
+        }
+    }
 }
