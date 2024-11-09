38f992d85c00f0a19dad70f837ac47b8b4565bbb
k9 <olafkrawczyk13@icloud.com>
2024-11-09 17:31:17 +0100

    Coding Exercise 11: Equality Printer

--- Section 2 Expressions, Statements & More/CodingExercise11.java
+++ Section 2 Expressions, Statements & More/CodingExercise11.java
@@ -1,2 +1,23 @@
 public class CodingExercise11 {
+
+    public static void main(String[] args) {
+        printEqual(1,2,3);
+    }
+
+    public static void printEqual(int a, int b, int c) {
+        if(a < 0 || b < 0 || c < 0 ) {
+            System.out.println("Invalid Value");
+        } else {
+            if (a == b || a == c || b == c) {
+                if (a == c && b == c) {
+                    System.out.println("All numbers are equal");
+                } else {
+                    System.out.println("Neither all are equal or different");
+                }
+            } else {
+                System.out.println("All numbers are different");
+            }
+        }
+    }
+
 }
