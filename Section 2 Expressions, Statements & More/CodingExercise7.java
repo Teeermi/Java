5d1730f8d402907fba538e22c39cedf7a409c437
k9 <olafkrawczyk13@icloud.com>
2024-11-09 13:11:59 +0100

    Coding Exercise 7: Equal Sum Checker

---
+++ Section 2 Expressions, Statements & More/CodingExercise7.java
@@ -0,0 +1,17 @@
+public class CodingExercise7 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static boolean hasEqualSum(int one, int two, int three) {
+        if(one + two == three) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+}
