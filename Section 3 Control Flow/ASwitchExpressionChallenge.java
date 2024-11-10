db2a715a2714bc020ebb361f6852da86ffb8c814
k9 <olafkrawczyk13@icloud.com>
2024-11-10 20:45:34 +0100

    A Switch Expression Challenge

---
+++ Section 3 Control Flow/ASwitchExpressionChallenge.java
@@ -0,0 +1,21 @@
+public class ASwitchExpressionChallenge {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printDayOfWeek(int day) {
+        String dayOfTheWeek = switch (day) {
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid day";
+        };
+
+        System.out.println(day + " " + dayOfTheWeek);
+
+    }
+}
