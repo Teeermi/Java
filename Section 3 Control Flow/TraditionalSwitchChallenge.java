1e2b3a128eed5c618e8fab02e1c4abadc0b77ad9
k9 <olafkrawczyk13@icloud.com>
2024-11-10 20:40:55 +0100

    Traditional Switch Challenge

---
+++ Section 3 Control Flow/TraditionalSwitchChallenge.java
@@ -0,0 +1,23 @@
+public class TraditionalSwitchChallenge {
+    public static void main(String[] args) {
+        char code = 'A';
+
+        switch (code) {
+            case 'A':
+                System.out.println("Able");
+                break;
+                case 'B':
+                    System.out.println("Baker");
+                    break;
+                    case 'C':
+                        System.out.println("Charlie");
+                        break;
+                        case 'D':
+                            System.out.println("Dog");
+                            break;
+                                case 'E':
+                                    System.out.println("Easy");
+        }
+
+    }
+}
